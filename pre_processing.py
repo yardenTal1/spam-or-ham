41949443683b1d72b91cdb84045b7eb4bb35aaf4
Elad Shoham <elad.shoham@cs.huji.ac.il>
2019-08-25 13:23:20 +0300

    change names and organize

--- pre_processing.py
+++ pre_processing.py
@@ -7,6 +7,10 @@ from nltk.wsd import lesk
 import enchant
 
 
+PHONE_NUMBER = 'phonenumber'
+OTHER_NUMBER = 'othernumber'
+
+
 def read_data():
     messages = pd.read_csv("./spam.csv", encoding='latin-1')
     # Drop the extra columns and rename columns
@@ -32,24 +36,28 @@ def is_number(in_string):
     return all(char.isdigit() for char in in_string)
 
 
-def translate_slang_and_lower(message):
+def pre_process_msg(message):
+    # remove punctuation # TODO check if use ' ' instead of ''
     message = message.translate(str.maketrans('', '', string.punctuation))
+    # translate slang, and lower words
     words = [slang_dict[word.lower()].lower() if word.lower() in slang_dict else word.lower() for word in message.split()]
+    # translate numbers to phonenumber and othernumber
+    words = [handle_nembers(word) if has_number(word) and handle_nembers(word) is not None else word for word in words]
+
     message = " ".join(words)
     return message
 
 
-def cleanText(message):
+def clean_text(message):
     #TODO remove words that are not in english? what about 'goooooood' for example?
 
     # remove punctuation
     words = []
     sentense = message.split()
     for word in sentense:
-        if has_number(word):
-            word = handle_nembers(word)
-            if word is None:
-                continue
+        if word in [PHONE_NUMBER, OTHER_NUMBER]:
+            words.append(word)
+            continue
         else:
             if not english_dict.check(word):
                 print(word)
@@ -75,22 +83,25 @@ def find_sense(word, sentense):
         if new_word is not None and len(new_word) > 0:
             word = new_word[0]._name.split('.')[0]  # TODO
     return word
-find_sense('call', 'please call me back as soon as possible i have great news :)')
+
+
 def handle_nembers(number):
     number = number.translate(str.maketrans('', '', ','))
     number = number.translate(str.maketrans('', '', '-'))
     if is_number(number):
         if len(number) >= 7:
-            return 'phonenumber'
-        return 'othernumber'
+            return PHONE_NUMBER
+        return OTHER_NUMBER
     return None # TODO maybe add other signs - dollar, euro, etc
 
 
-def pre_processing_stage_one(message):
-    message_tokens = nltk.word_tokenize(message)
+def pre_process_data(data):
+    data['text'] = data['text'].apply(pre_process_msg)
+    data['text'] = data['text'].apply(clean_text)
 
 
 if __name__ == "__main__":
+    # General variables
     nltk_stop_words = set(nltk.corpus.stopwords.words('english'))
     signs_list = [',', '/', '.', '"', "'", '?', '\\', ':', '(', ')', '*', '-', '=', '+', '&', '^', '$', '%', '#', '@',
                   '!', '`', '~', "'s"]
@@ -99,8 +110,8 @@ if __name__ == "__main__":
     english_dict = enchant.Dict("en_US")
     slang_dict = create_noslang_dict()
 
-    msg = read_data()
-    msg['text'] = msg['text'].apply(translate_slang_and_lower)
-    msg['text'] = msg['text'].apply(cleanText)
+    # Actual code
+    data = read_data()
+    data = pre_process_data(data)
 
     pass # TODO do lesk just after we remove un english words
