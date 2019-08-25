4b7ca2b1ba750b5d8fc48afb7cfe10d5a8bf18ce
yardenTal1 <yardental55@gmail.com>
2019-08-25 17:08:02 +0300

    NB algorithm

--- pre_processing.py
+++ pre_processing.py
@@ -5,7 +5,13 @@ import pandas as pd
 import string
 from nltk.wsd import lesk
 import enchant
-
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 PHONE_NUMBER = 'phonenumber'
 OTHER_NUMBER = 'othernumber'
@@ -99,6 +105,62 @@ def pre_process_data(data):
     data['text'] = data['text'].apply(pre_process_msg)
     data['text'] = data['text'].apply(clean_text)
 
+    return data
+
+
+def prepare_data_for_classify(data):
+    # assigned label 1 if spam and 0 if ham
+    data['label'] = data['category'].apply(lambda x: 0 if x =='ham' else 1)
+    # Split data into training and testing sets
+    x_train, x_test, y_train, y_test = train_test_split(data['text'], data['label'], random_state = 1)
+    return x_train, x_test, y_train, y_test
+
+
+def bag_of_words(x_train, x_test):
+    cv = CountVectorizer(strip_accents='ascii', token_pattern = u'(?ui)\\b\\w * [a - z] +\\w *\\b', lowercase = True,
+                         stop_words ='english')
+    x_train_cv = cv.fit_transform(x_train)
+    x_test_cv = cv.transform(x_test)
+    return x_train_cv, x_test_cv, cv
+
+
+def investigate_data(x_cv, cv):
+    word_freq_df = pd.DataFrame(x_cv.toarray(), columns=cv.get_feature_names())
+    top_words_df = pd.DataFrame(word_freq_df.sum()).sort_values(0, ascending=False)
+
+
+def multinomial_naive_bayes_classifier(x_train_cv, x_test_cv):
+    naive_bayes = MultinomialNB()
+    naive_bayes.fit(x_train_cv, y_train)
+    predictions = naive_bayes.predict(x_test_cv)
+    return predictions
+
+
+def print_results(y_test, predictions):
+    print('Accuracy score: ', accuracy_score(y_test, predictions))
+    print('Precision score: ', precision_score(y_test, predictions))
+    print('Recall score: ', recall_score(y_test, predictions))
+
+
+def investigate_score(y_test, predictions):
+    cm = confusion_matrix(y_test, predictions)
+    sns.heatmap(cm, square=True, annot=True, cmap='RdBu', cbar = False, xticklabels = ['ham', 'spam'], yticklabels = ['ham', 'spam'])
+    plt.xlabel('true label')
+    plt.ylabel('predicted label')
+    plt.show()
+
+
+def investigate_misses(x_test, y_test, predictions):
+    testing_predictions = []
+    for i in range(len(x_test)):
+        if predictions[i] == 1:
+            testing_predictions.append('spam')
+        else:
+            testing_predictions.append('ham')
+    check_df = pd.DataFrame({'actual_label': list(y_test), 'prediction': testing_predictions, 'text':list(x_test)})
+    check_df.replace(to_replace=0, value='ham', inplace = True)
+    check_df.replace(to_replace=1, value='spam', inplace = True)
+
 
 if __name__ == "__main__":
     # General variables
@@ -113,5 +175,19 @@ if __name__ == "__main__":
     # Actual code
     data = read_data()
     data = pre_process_data(data)
+
+    x_train, x_test, y_train, y_test = prepare_data_for_classify(data)
+    x_train_cv, x_test_cv, cv = bag_of_words(x_train, x_test)
+
+    # investigate data
+    investigate_data(x_train_cv, cv)
+    investigate_data(x_test_cv, cv)
+
+    predictions = multinomial_naive_bayes_classifier(x_train_cv, x_test_cv)
+
+    # investigate results
+    print_results(y_test, predictions)
+    investigate_score(y_test, predictions)
+    investigate_misses(x_test, y_test, predictions)
 
     pass # TODO do lesk just after we remove un english words
